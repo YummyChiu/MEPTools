f85fe7444885d321f5c67f228b87494c0020392e
YummyChiu <duanquanzhao@gmail.com>
2017-05-26 09:12:23 +0800

    添加LinkCommand

---
+++ MEPTools/Link/LinkCommand.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+
+namespace MEPTools.Link
+{
+
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    class LinkCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+
+            return Result.Succeeded;
+
+        }
+    }
+}
